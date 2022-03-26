ce9d2ac4849152253ba122ea52f53d126681d1cd
Sheena Bonilla <sheena.myb@gmail.com>
2022-03-26 00:02:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json"
+//         "Accept": "application/json"
+//     },
+//     /* Your data goes here */
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle"
+//     })
+// };
+let formData = {
+    name: "Solo",
+    email: "solo@gmail.com"
+}
+
+const configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept":  'application/json'
+    },
+    body: JSON.stringify(formData)
+};
+
+
+function submitData(formData) {
+ 
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        debugger;
+        document.body.innerHTML = object[ "id" ]
+    })
+    .catch(function(error) {
+        document.body.innerHTML = error.message
+    })
+}
